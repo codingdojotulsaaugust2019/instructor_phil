551ba655c61d67d301c42bfcf7d6fe15c0366419
Phil Krull <pkrull@codingdojo.com>
2019-09-17 12:06:20 -0500

    adding model manager to django relationships

--- _python/django/DjangoRelationships/apps/firstapp/models.py
+++ _python/django/DjangoRelationships/apps/firstapp/models.py
@@ -2,16 +2,47 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    #objects object
     name = models.CharField(max_length=45)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    #objects object
     # one to many relationship
     # owns = [Car objects]
     # many to many relationship
     # back_seat_driver = [Car objects]
     def __repr__(self):
         return f'<User object: name={self.name} id={self.id}>'
+
+        
+
+class CarManager(models.Manager):
+    def validate_car(self, form_data):
+        print(form_data)
+        print('*'*20 + 'in models' + '*'*20)
+        errors = []
+        if len(form_data['name']) < 1:
+            # name was not there
+            errors.append('Name must be present!')
+        if not form_data['name'].isalpha():
+            # name contains numbers
+            errors.append('Name can not have numbers!')
+        owner = User.objects.filter(id=form_data['owner_id'])
+        if len(owner) < 1:
+            errors.append('Car must have an owner!')
+
+        # how do know if we passed the validations?
+        if(len(errors) < 1):
+            # passed validations
+            # save the car
+            car = self.create(name=form_data['name'], owner=owner[0])
+            response_to_views = {'status': True, 'car': car}
+            return response_to_views
+        else:
+            # failed validaions
+            # send the messages to views
+            response_to_views = {'status': False, 'errors': errors}
+            return response_to_views
 
 class Car(models.Model):
     name = models.CharField(max_length = 45)
@@ -22,6 +53,7 @@ class Car(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    objects = CarManager()
     def __repr__(self):
         print(f'<Car object: {self.name} passengers are:>')
         print(self.passengers)
--- _python/django/DjangoRelationships/apps/firstapp/views.py
+++ _python/django/DjangoRelationships/apps/firstapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from .models import User, Car
 
 # Create your views here.
@@ -12,7 +13,20 @@ def index(request):
 def add_car(request):
     if request.method == 'POST':
         print(request.POST)
-        Car.objects.create(name = request.POST['name'], owner = User.objects.get(id = request.POST['owner_id']))
+        print('*'*20 + 'in views' + '*'*20)
+        response_from_models = Car.objects.validate_car(request.POST)
+        print('*'*20 + 'in views response from model' + '*'*20)
+        print(response_from_models)
+        if response_from_models['status'] == True:
+            # car was saved
+            pass
+        else:
+            # failed the validations
+            # add errors to messages
+            for error in response_from_models['errors']:
+                messages.error(request, error)
+            
+        # Car.objects.create(name = request.POST['name'], owner = User.objects.get(id = request.POST['owner_id']))
 
     return redirect("/")
 
