09db96986ef7a03aab79d35d49b2dceb984f9d0c
Claire Elliott <celliott@codingdojo.com>
2019-11-08 13:57:13 -0600

    added Angular services

--- mean/mongoose/1955/public/src/app/app.component.ts
+++ mean/mongoose/1955/public/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpService } from './http.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private _httpService: HttpService){}
   title = 'Phil';
+  bBoomer = [];
+
+  ngOnInit(){
+    this.getUsersFromService();
+  }
+
+  getUsersFromService(){
+    this._httpService.getUsers().subscribe(data =>{
+      console.log(data);
+      this.bBoomer = data['users']
+    })
+  }
 }
--- mean/mongoose/1955/public/src/app/app.module.ts
+++ mean/mongoose/1955/public/src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { HttpService } from './http.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
